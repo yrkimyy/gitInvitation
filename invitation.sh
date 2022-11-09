037d19458a962d0464b2ad5426ab8ca2cbf8b736
Kelly <68624696+yrkimyy@users.noreply.github.com>
2022-11-09 12:09:46 +0900

    팀초대로 변경

---
+++ invitation.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+while read line;
+do
+
+
+item=$(curl \
+  -sL -X PUT \
+  -H "Authorization: Token ---" \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/orgs/cs-devops-bootcamp/teams/devops-3/memberships/$line \
+    -d '{"role":"member"}'
+)
+
+echo $item
+
+done < file.txt > test.json
